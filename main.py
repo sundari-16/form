c8c1fe2654e0e82775370cdceea66e2c6b102f5b
sundari-16 <ssundarisenthil16@gmail.com>
2025-07-17 16:53:28 -0700

    formapi

---
+++ main.py
@@ -0,0 +1,114 @@
+import mysql.connector
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    Name: str
+    phno: str
+    email: str
+    password: str
+
+@app.post("/reg")
+def user(data: Item):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="office",
+        port=3307
+    )
+    mypost = mydb.cursor()
+    mypost.execute(
+        "insert into user (name, phno, email, password) values ('"
+        + data.Name + "', '" + data.phno + "', '" + data.email + "', '" + data.password + "')"
+    )
+    mydb.commit()
+    mydb.close()
+    return {"message": "registered successfully"}
+
+
+class LoginItem(BaseModel):
+    email: str
+    password: str
+
+@app.post("/login")
+def login(data: LoginItem):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="office",
+        port=3307
+    )
+    mypost = mydb.cursor()
+    mypost.execute(
+        "select * from user where email='" + data.email + "' AND password='" + data.password + "'"
+    )
+    r = mypost.fetchall()
+    mydb.close()
+    if r:
+        return {"message": "Success"}
+    else:
+        return {"message": "Invalid"}
+
+@app.get("/view")
+def view():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="office",
+        port=3307
+    )
+    mypost = mydb.cursor(dictionary=True)
+    mypost.execute("select * from user")
+    r = mypost.fetchall()
+    mydb.close()
+    return r
+
+
+class UpdateItem(BaseModel):
+    name: str
+
+@app.post("/update/{id}")
+def update(data: UpdateItem, id: int):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="office",
+        port=3307
+    )
+    mypost = mydb.cursor()
+    mypost.execute("update user set name='" + data.name + "' where id=" + str(id))
+    mydb.commit()
+    mydb.close()
+    return {"message": "Updated"}
+
+
+@app.delete("/del/{id}")
+def delete(id: int):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="office",
+        port=3307
+    )
+    mypost = mydb.cursor()
+    mypost.execute("delete from user where id=" + str(id))
+    mydb.commit()
+    mydb.close()
+    return {"message": "Deleted"}
